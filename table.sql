269e33925c12de140e82695a6daaa1dee177d713
Ncebakazi <ncebakazi414@gmail.com>
2021-09-02 16:10:18 +0200

    test db test

--- table.sql
+++ table.sql
@@ -2,7 +2,6 @@ create table users (
 	id serial not null primary key,
 	name text not null,
     counter int
-
  );
 
 -- create table cats (id serial primary key, cat_name text not null, spotted_count int not null);
